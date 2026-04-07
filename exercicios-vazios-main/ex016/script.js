1cab83cc1bfc76bc4bb1030f51701b93942b5e14
Gabriel de Souza Almeida <123323173+gabriel-sp16@users.noreply.github.com>
2026-04-06 21:27:18 -0300

    Add files via upload

---
+++ exercicios-vazios-main/ex016/script.js
@@ -0,0 +1,10 @@
+let numero = Number(prompt("Digite o valor a ser multiplicado: "))
+let nu
+let contador =0
+let multiplicador = numero * ++contador;
+let resultado=multiplicador
+
+
+if (numero <= 10) {
+    alert(numero " X " " = " resultado);
+}
