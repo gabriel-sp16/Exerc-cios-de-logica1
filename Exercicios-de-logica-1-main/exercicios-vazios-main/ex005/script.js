18df60574b50617616e1ba15c3e788f2ba9c3ba7
Gabriel de Souza Almeida <123323173+gabriel-sp16@users.noreply.github.com>
2026-06-18 21:14:47 -0300

    Delete Exercicios-de-logica-1-main/exercicios-vazios-main directory

--- Exercicios-de-logica-1-main/exercicios-vazios-main/ex005/script.js
+++
@@ -1,4 +0,0 @@
-let numero1 = Number(prompt("Digite o primeiro numero inteiro para a soma dos numeros:"));
-let numero2 = Number(prompt("Digite o segundo numero inteiro para a soma dos numeros:"));
-let soma = numero1 + numero2;
-alert("A o resultado da soma é: " + soma);
--- Exercicios-de-logica-1-main/exercicios-vazios-main/ex015/script.js
+++
@@ -1,4 +0,0 @@
-let base = Number(prompt("Digite o valor da base do retangulo: "))
-let altura = Number(prompt("Digite o valor da altura do retangulo:"))
-let area = base * altura;
-alert("A area do retangulo é: " + area)
